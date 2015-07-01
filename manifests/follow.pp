98c748383d07de9874dace45064d77878e269d23
Iszak Bryan <iszak.bryan@secretsales.com>
2015-07-01 08:32:45 +0100

    Reworked follow
    Removed the docblock as the license is clearly in the index file, also
    added dependencies on the service/install classes to ensure properly
    setup logentries before following

--- manifests/follow.pp
+++ manifests/follow.pp
@@ -1,43 +1,24 @@
+# Type: logentries::follow
 #
-# Author:: Mohamed Hadrouj
-# Module Name:: logentries
-# Class:: logentries::follow
+# This type configures logentries follow rules
 #
+# Parameters:
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Sample Usage:
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# logentries::follow { '/var/log/syslog': }
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 define logentries::follow(
-  $display_name='',
-  $log_type='',
+  $display_name = undef,
+  $log_type     = undef,
 ) {
+  $log_file = regsubst($name, '[;\\/:*?\"<>|&]', '_', 'G')
 
-  if ($display_name != '') {
-    $name_flag = "--name '${display_name}'"
-  }
-
-  if ($log_type != '') {
-    $type_flag = "--type '${log_type}'"
-  }
-
-  $log_file=regsubst($name, '[;\\/:*?\"<>|&]', '_', 'G')
   exec { "le_follow_${name}":
-    command => "le follow ${name} ${name_flag} ${type_flag}",
-    unless  => "python -c \"if '${display_name}' not in '''\$(le whoami)''':exit(1)\"",
-    # "le followed ${name}",
-    path    => '/usr/bin/:/bin/',
-    require => [Package['logentries'], Exec['le_register']],
-    notify  => Service['logentries'],
+    command => "le follow ${name} ${display_name} ${log_type}",
+    unless  => "le followed ${name}",
+    require => Class['logentries::install'],
+    notify  => Class['logentries::service'],
     timeout => 30,
   }
 }
