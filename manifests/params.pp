e283c8963ff3f4d0536dc50a95605d5b08a7b9ae
Iszak Bryan <iszak.bryan@secretsales.com>
2015-07-01 08:27:05 +0100

    Added parameters for services, package, yum
    Centralising most of the switch operating system logic to a params.pp
    file allowing users to override defaults

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,41 @@
+# Class: logentries::params
+#
+# This class manages logentries parameters
+#
+# Parameters:
+#
+# Sample Usage:
+#
 class logentries::params {
-  $manage_package_repo = false
+  case $::operatingsystem {
+    'RedHat', 'CentOS', 'Fedora': {
+      $package_name    = ['python-setproctitle', 'python-simplejson', 'logentries']
+      $package_require = Yum['logentries']
+
+      $yum_baseurl = "http://rep.logentries.com/rh/${::architecture}"
+    }
+    'Amazon': {
+      $package_name    = ['python27-simplejson', 'logentries']
+      $package_require = Yum['logentries']
+
+      $yum_baseurl = "http://rep.logentries.com/amazon\${releasever}/\${basearch}"
+    }
+    'Ubuntu', 'Debian': {
+      $package_name = ['logentries']
+      $package_require = Apt::Source['logentries']
+    }
+    default: {
+      fail("Unsupported osfamily ${::osfamily}")
+    }
+  }
+  $package_manage = true
+  $package_ensure = present
+
+
+  $service_name   = 'logentries'
+  $service_manage = true
+  $service_enable = true
+  $service_ensure = running
+
+  $repo_manage    = true
 }
